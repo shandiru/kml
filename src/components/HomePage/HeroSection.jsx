86afad4a06b1af20130f50e371a78e4c6140373d
shandiru <msddiru@gmail.com>
2026-06-11 19:04:16 +0530

    added

--- src/components/HomePage/HeroSection.jsx
+++ src/components/HomePage/HeroSection.jsx
@@ -21,7 +21,7 @@ export default function HeroSection() {
       {/* ✅ Video background */}
       <video
         className="absolute inset-0 w-full h-full object-cover z-0"
-        src="/newhero.mp4"
+        src="/newheros.mp4"
         autoPlay
         loop
         muted
